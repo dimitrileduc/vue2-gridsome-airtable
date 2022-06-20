a00b6ce1e800187848f2fd1173a17f54e0891e69
Dimitri Leduc <dimitrileduc@MacBook-Pro-de-Dimitri.local>
2022-06-20 13:58:32 +0200

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,46 @@
+<template>
+    <Layout>
+        <section>
+            <g-link
+                v-for="edge in $page.allProduct.edges"
+                :key="edge.node.id"
+                :to="edge.node.path"
+                class="product"
+            >
+                <p>{{ edge.node.title }}</p>
+            </g-link>
+        </section>
+    </Layout>
+</template>
+
+<!-- Query from local GraphQL data layer. -->
+<page-query>
+query Products {
+      allProduct {
+        edges {
+          node {
+            id,
+            title,
+            path,
+            content,
+            
+          }
+        }
+      }
+    }
+
+</page-query>
+
+<script>
+export default {
+    metaInfo: {
+        title: "Hello, world!",
+    },
+};
+</script>
+
+<style>
+.home-links a {
+    margin-right: 1rem;
+}
+</style>
---
+++ src/templates/Product.vue
@@ -0,0 +1,29 @@
+<template>
+    <Layout>
+        <h1>{{ $page.product.title }}</h1>
+        <p>
+            {{ $page.product.content }}
+        </p>
+    </Layout>
+</template>
+
+<script>
+export default {
+    metaInfo: {
+        title: "Product",
+    },
+};
+</script>
+
+<!-- Query from local GraphQL data layer. -->
+<page-query>
+query Product ($path: String!) {
+      product(path: $path) {
+        id,
+        title,
+        path,
+        content,
+      }
+    }
+
+</page-query>
